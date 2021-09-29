888448c4688a60e077c69858a960dc0d785a211a
zolotilov <zolotilov@ugpa.ru>
2021-09-29 14:40:16 +0300

    add plugins xunit.2.4.1.nupkg and others

---
+++ Tests/SettingsTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace XmlCompare.Tests
+{
+    public class SettingsTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(13, Add());
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(4, Add2());
+        }
+
+        int Add() => 4 + 9;
+        int Add2() => 2 + 2;
+    }
+}
